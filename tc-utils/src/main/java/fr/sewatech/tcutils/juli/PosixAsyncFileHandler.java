c4f2b8b2488b7c086ac3bb5f14cf09cd6d129e74
Alexis Hassler <alexis.hassler@sewatech.fr>
2015-11-28 10:37:12 +0100

    Version 0.4.0

--- tc-utils/src/main/java/fr/sewatech/tcutils/juli/PosixAsyncFileHandler.java
+++ tc-utils/src/main/java/fr/sewatech/tcutils/juli/PosixAsyncFileHandler.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2015 Sewatech
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package fr.sewatech.tcutils.juli;
 
 import fr.sewatech.tcutils.commons.PosixFiles;
@@ -35,7 +50,7 @@ public class PosixAsyncFileHandler extends AsyncFileHandler {
 
     private File guessLogFile() {
         String className = this.getClass().getName();
-        File directory = new File(getProperty(className + ".directory", "logs"));;
+        File directory = new File(getProperty(className + ".directory", "logs"));
         String prefix = getProperty(className + ".prefix", "juli.");
         boolean rotatable = Boolean.parseBoolean(getProperty(className + ".rotatable", "true"));
         String suffix = getProperty(className + ".suffix", ".log");
